989386a3448a5dc8c3f92a11470dfd0c607bd9a0
Adrian Etenyi <etadriano2@gmail.com>
2020-04-11 18:12:39 +0300

    import the needed classes to my-profile component

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User} from '../user';
+import { Repos } from '../repos';
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
