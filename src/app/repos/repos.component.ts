20f0da06d5347f72d6bc223f1884573087d4ca64
Adrian Etenyi <etadriano2@gmail.com>
2020-04-11 19:32:31 +0300

    add component for repositories

---
+++ src/app/repos/repos.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import{User} from '../user';
+import { Repos } from '../repos';
+import { ProfileService } from '../services/profile.service';
+import{HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-repos',
+  templateUrl: './repos.component.html',
+  styleUrls: ['./repos.component.css'],
+
+  providers:[ProfileService]
+})
+
+
+export class ReposComponent implements OnInit {
+
+  public username ='Brightmuk';
+  repos:Repos;
+  user:User;
+  constructor(private profileService:ProfileService,public repositoryService:ProfileService) { }
+
+  ngOnInit()  {
+    this.profileService.getProfileInfo(this.username);
+      this.user = this.profileService.user;
+
+      this.repositoryService.getProfileRepo(this.username);
+      this.repos = this.repositoryService.repos;
+
+  }
+
+}
