e0942d1a14295410e60ec747ce74c0c0f7cc8e17
Adrian Etenyi <etadriano2@gmail.com>
2020-04-11 18:04:36 +0300

    add API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
