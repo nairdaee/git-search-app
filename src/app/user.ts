5fe55fdbb3f6ba0f8b9959e85c1c11ae03d8f9f6
Adrian Etenyi <etadriano2@gmail.com>
2020-04-11 17:39:45 +0300

    create user,repository and search class; add initial values for variables in user class

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+
+    constructor(public name:string, public login: string,  public avatar_url:string,
+        public public_repos:number, public followers:string, public following:string,
+        public email:string, public location:string, public html_url:string,
+        public repos_url:string, public created_at:Date){
+      }
+
+      
+}
