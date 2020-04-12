dc17cf9bc88580007c8c8d250942212cd35cb520
Adrian Etenyi <etadriano2@gmail.com>
2020-04-12 19:23:27 +0300

    make live site deployable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl:'https://api.github.com/users/',
+  apikey: '?access_token=b00c74797374367ce9f3ab5c638e578390cd7a68',
+  repoapikey:'/repos?access_token=b00c74797374367ce9f3ab5c638e578390cd7a68'
 };
